b469471ab5b6c2677adef9e2a725107127180183
smjubaeair <smjubaeairhossen@gmail.comgit config --global user.namel smjubaeairm>
2022-12-20 20:24:35 +0600

    yuuytyu

--- src/component/EmploysCrud.jsx
+++ src/component/EmploysCrud.jsx
@@ -48,7 +48,7 @@ const EmploysCrud = () => {
           ></input>
           <span class="input-group-text" id="basic-addon1">
             <button onClick={handleClick} type="button" class="btn btn-primary">
-              Filter
+              Seaarch
             </button>
           </span>
         </div>
